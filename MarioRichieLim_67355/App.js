e97266b6fdb615ab8a76678db0a18d5682cabd6f
Twilight <mario.richie.lim@gmail.com>
2024-09-04 10:56:47 +0700

    Week 1 Tugas

--- MarioRichieLim_67355/App.js
+++ MarioRichieLim_67355/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mario Richie Lim - 00000067355</Text>
       <StatusBar style="auto" />
     </View>
   );
