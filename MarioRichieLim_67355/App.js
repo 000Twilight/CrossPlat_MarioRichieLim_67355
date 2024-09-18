b9fabaecfea4520cd240e578bf46fee67b67b9a2
Twilight <mario.richie.lim@gmail.com>
2024-09-18 10:22:02 +0700

    part 1

--- MarioRichieLim_67355/App.js
+++ MarioRichieLim_67355/App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import styles from './Styles';
+import data from './data.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      {/* <Text>Testing</Text> */}
+      <View style={styles.contentContainer}>
+        {data.map((item) => (
+          <View style={styles.contentContainer}>
+            <Image source={{ uri: item.photo_url }} style={styles.photo} />
+            <Text style={styles.textSize}>{item.name}</Text>
+            <Text style={styles.textSize}>{item.email}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
