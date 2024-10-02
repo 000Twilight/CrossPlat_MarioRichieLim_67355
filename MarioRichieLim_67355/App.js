ec78952e2989b5103578f898c6f8b1888cc332c8
Twilight <mario.richie.lim@gmail.com>
2024-10-02 08:43:29 +0700

    .

--- MarioRichieLim_67355/App.js
+++ MarioRichieLim_67355/App.js
@@ -1,13 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import app_styles from './Styles/App.styles';
+import * as React from 'react';
+import { Text, View, ScrollView, useState } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import data from './data.json';
+import app_styles from './App.styles';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Email from './Screens/Email';
+// import HomeScreen from './Screens/HomeScreen';
+// import { theme } from './Theme';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  // const [isDarkTheme, setIsDarkTheme] = useState(false);
+  // const toggleTheme = () => setIsDarkTheme(!isDarkTheme);
+  // const currentTheme = isDarkTheme ? theme.dark : theme.light;
+
+  // const styles = stylesFunc(dark);
+
   return (
-    <View style={app_styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* <Stack.Screen name="HomeScreen" component={HomeScreen} />
+          <Stack.Screen name="Email" component={Email} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
