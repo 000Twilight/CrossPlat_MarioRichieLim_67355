0487e36255559273a771303a1beebc4cbffe0c4a
Twilight <mario.richie.lim@gmail.com>
2024-10-02 08:38:50 +0700

    .

--- MarioRichieLim_67355/styles/App.styles.js
+++ MarioRichieLim_67355/styles/App.styles.js
@@ -1,9 +1,75 @@
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
-  
+import { StyleSheet } from 'react-native';
+
+const app_styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 10,
+  },
+  contentContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+  // title: {
+  //   textAlign: 'center',
+  //   marginVertical: 10,
+  //   color: 'black',
+  // },
+  // card: {
+
+  // },
+  // userList: {
+
+  // },
+  // avatar: {
+
+  // },
+  // boldText: {
+
+  // },
+});
+
+export default app_styles;
+
+// import { StyleSheet } from 'react-native';
+// import { DefaultTheme, DarkTheme } from 'react-native-paper';
+
+// // Define the combined theme and styles
+// export const combinedThemeAndStyles = (isDarkTheme) => {
+//   const customTheme = isDarkTheme ? {
+//     ...DarkTheme,
+//     colors: {
+//       primary: '#191970',
+//       background: '#333333',
+//       text: 'white',
+//     },
+//   } : {
+//     ...DefaultTheme,
+//     colors: {
+//       primary: '#ffffff',
+//       background: '#f8f8f8',
+//       text: 'black',
+//     },
+//   };
+
+//   // Create styles based on the theme
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       marginTop: 10,
+//       backgroundColor: customTheme.colors.background,
+//     },
+//     contentContainer: {
+//       alignItems: 'center',
+//       justifyContent: 'center',
+//     },
+//     card: {
+//       width: '80%',
+//       marginBottom: 10,
+//       borderRadius: 8,
+//       backgroundColor: customTheme.colors.primary,
+//     },
+//   });
+
+//   return { customTheme, styles };
+// };
