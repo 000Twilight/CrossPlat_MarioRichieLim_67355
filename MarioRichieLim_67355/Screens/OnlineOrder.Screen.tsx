b8a555a749a29ecffdba1c8d432dbbf0b02ddc25
Twilight <mario.richie.lim@gmail.com>
2024-11-25 23:18:57 +0700

    .

--- MarioRichieLim_67355/Screens/OnlineOrder.Screen.tsx
+++ MarioRichieLim_67355/Screens/OnlineOrder.Screen.tsx
@@ -1,31 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
 import CustomHeader from '../Components/CustomHeader';
 import HorizontalScroll from '../Components/HorizontalScroll';
 import FeaturesCards from '../Components/FeaturesCard';
+import online_order_styles from '../Styles/OnlineOrder.style';
 
 const OnlineOrderScreen = () => {
     return (
-        <View style={{ flex: 1, backgroundColor: 'white' }}>
+        <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
             <CustomHeader title="Online Order" />
-            <View style={styles.paddingApp}>
+            <View style={online_order_styles.paddingApp}>
                 <HorizontalScroll />
                 <FeaturesCards />
-                <Text style={styles.sectionTitle}>Online Order</Text>
             </View>
-        </View>
+            <View style={online_order_styles.orderNowSection}>
+                <Text style={online_order_styles.orderNowTitle}>Order Now</Text>
+                <TouchableOpacity style={online_order_styles.cardOrder}>
+                    <Image
+                        source={{ uri: 'https://via.placeholder.com/600x400' }}
+                        style={online_order_styles.imageOrder}
+                    />
+                    <View style={online_order_styles.cardContent}>
+                        <Text style={online_order_styles.cardTitle}>Salad Buah Murah, Cuma 10k</Text>
+                        <Text style={online_order_styles.cardSubtitle}>Ad - Salad Buah Eni</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+            <View style={online_order_styles.paddingApp}>
+                <Text style={online_order_styles.sectionTitle}>24 Hour</Text>
+
+            </View>
+
+            <View style={online_order_styles.spaceDown}></View>
+        </ScrollView>
     );
 };
-
-const styles = StyleSheet.create({
-    paddingApp: {
-        padding: 16,
-    },
-    sectionTitle: {
-        fontSize: 20,
-        fontWeight: '600',
-        marginTop: 20,
-    },
-});
 
 export default OnlineOrderScreen;
---
+++ MarioRichieLim_67355/Styles/OnlineOrder.style.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+
+const online_order_styles = StyleSheet.create({
+    paddingApp: {
+        padding: 16,
+    },
+    sectionTitle: {
+        fontSize: 20,
+        fontWeight: '600',
+        marginTop: 20,
+    },
+
+
+    orderNowSection: {
+        marginTop: 10,
+        padding: 16,
+        backgroundColor: '#f8f8f8',
+    },
+    orderNowTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    cardOrder: {
+        flexDirection: 'column',
+        borderRadius: 8,
+        overflow: 'hidden',
+        // elevation: 2,
+    },
+    imageOrder: {
+        width: 100,
+        height: 100,
+    },
+    cardContent: {
+        padding: 10,
+        justifyContent: 'center',
+    },
+    cardTitle: {
+        fontSize: 16,
+        fontWeight: '600',
+    },
+    cardSubtitle: {
+        fontSize: 14,
+        color: '#777',
+        marginTop: 4,
+    },
+
+    spaceDown: {
+        padding: 50,
+    },
+});
+
+export default online_order_styles;
