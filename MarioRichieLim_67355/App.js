b5fb0441dd38f40f2ea2315f1d755ca267220898
Twilight <mario.richie.lim@gmail.com>
2024-12-12 14:54:07 +0700

    .

--- MarioRichieLim_67355/App.js
+++ MarioRichieLim_67355/App.js
@@ -2,6 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const addData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Mario Richie",
+        last: "Lim",
+        born: 2004
+      });
+
+      if (Platform.OS === "android") {
+        console.log("Document written from phone with ID: ", docRef.id);
+      } else {
+        console.log("Document written with ID: ", docRef.id)
+      }
+
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ MarioRichieLim_67355/Firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD93V00R-pZ6NkVnmFudN8Nn0QylQBDGSg",
+    authDomain: "learning-1-f7091.firebaseapp.com",
+    databaseURL: "https://learning-1-f7091.firebaseio.com",
+    projectId: "learning-1-f7091",
+    storageBucket: "learning-1-f7091.firebasestorage.app",
+    messagingSenderId: "1096052718638",
+    appId: "1:1096052718638:web:e1b7fc52326777146ab3bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage();
+
+export default firebaseConfig;
